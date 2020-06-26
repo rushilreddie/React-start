965cf8011f038174193a21bf5133724eb5687e6e
Rushil <48923066+rushilreddie@users.noreply.github.com>
2020-06-26 13:16:39 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Example of lists in react</h1>
+    <p> This is an unordered list </p>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
