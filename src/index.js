4bf71b3c1e1c11631d3ac0540962d42b3635fc51
Rushil <48923066+rushilreddie@users.noreply.github.com>
2020-12-18 23:17:13 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const number = 7;
 ReactDOM.render(
   <div>
     <h1>Hello {`${fname} ${lname}`}!</h1>
-    <p> Your lucky number is {number} </p>
+    <p>rushil Your lucky number is {number} </p>
   </div>,
   document.getElementById("root")
 );
